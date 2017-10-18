4b7a1966eff9e4d0f86ce6cbb51e03251fc3c4f2
Gavriel Hass <gavhass@gmail.com>
2017-10-18 19:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ function happyHolidays () {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (Gav) {
+
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
-}
-
-
-function happyHolidayTo (holiday,name) {
-
-  return `${greeting}, ${firstName}!`
-}
